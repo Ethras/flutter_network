4faa643c4358eab0ea7fde6409b16ba1d9b18dcc
Ethras <vladimirmbassi@gmail.com>
2019-07-08 13:53:02 +0200

    EventChannel test

--- ios/Classes/SwiftFlutterNetworkPlugin.swift
+++ ios/Classes/SwiftFlutterNetworkPlugin.swift
@@ -2,12 +2,26 @@ import Flutter
 import UIKit
 import CoreTelephony
 
-public class SwiftFlutterNetworkPlugin: NSObject, FlutterPlugin {
-    private var cellState : CTCellularData
+public class SwiftFlutterNetworkPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+    private var cellularData : CTCellularData
+    private var eventSink: FlutterEventSink?
+    
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events;
+        return nil
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil
+        return nil
+    }
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_network", binaryMessenger: registrar.messenger())
+        let eventChannel = FlutterEventChannel(name: "com_ethras_flutter_network/stream", binaryMessenger: registrar.messenger())
         let instance = SwiftFlutterNetworkPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
+        eventChannel.setStreamHandler(instance)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -17,11 +31,21 @@ public class SwiftFlutterNetworkPlugin: NSObject, FlutterPlugin {
     }
     
     public override init() {
-        cellState = CTCellularData.init()
+        cellularData = CTCellularData.init()
+        eventSink = nil
+        super.init()
+        
+        cellularData.cellularDataRestrictionDidUpdateNotifier = { state in
+            if (self.eventSink != nil) {
+                print("cellularState changed! " + String(describing: state) )
+                let res = state == .restricted
+                self.eventSink!(res)
+            }
+        }
     }
     
     private func getCellularDataRestricted()  -> Bool {
-            print("CellularRestricted check : " + "\(cellState.restrictedState == .restricted ? "restricted" : cellState.restrictedState == .restrictedStateUnknown ? "unknown" : "unrestricted")")
-            return  cellState.restrictedState == .restricted
+        print("CellularRestricted check : " + "\(cellularData.restrictedState == .restricted ? "restricted" : cellularData.restrictedState == .restrictedStateUnknown ? "unknown" : "unrestricted")")
+        return  cellularData.restrictedState == .restricted
     }
 }
--- lib/flutter_network.dart
+++ lib/flutter_network.dart
@@ -5,6 +5,11 @@ import 'package:flutter/services.dart';
 class FlutterNetwork {
   static const MethodChannel _channel =
       const MethodChannel('flutter_network');
+  static const EventChannel  _stream  = const EventChannel('"com_ethras_flutter_network/stream"');
+
+  static  Stream<bool> dataRestrictedStateChanged() {
+    return _stream.receiveBroadcastStream().map((restricted) => restricted as bool);
+  }
 
   static Future<bool> get isCellularDataRestricted async {
     final bool isRestricted = await _channel.invokeMethod('isCellularDataRestricted');
