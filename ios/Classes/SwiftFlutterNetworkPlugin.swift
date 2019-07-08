595a8bcb065869df259fb692345ff8121f19a5d6
Ethras <vladimirmbassi@gmail.com>
2019-07-08 09:44:46 +0200

    Test 2

--- ios/Classes/SwiftFlutterNetworkPlugin.swift
+++ ios/Classes/SwiftFlutterNetworkPlugin.swift
@@ -3,31 +3,25 @@ import UIKit
 import CoreTelephony
 
 public class SwiftFlutterNetworkPlugin: NSObject, FlutterPlugin {
-    private static var currentState = CTCellularDataRestrictedState.restrictedStateUnknown
-    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_network", binaryMessenger: registrar.messenger())
         let instance = SwiftFlutterNetworkPlugin()
-        prepare()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "isCellularDataRestricted") {
-            result(SwiftFlutterNetworkPlugin.currentState == .restricted)
+            result(getCellularDataRestricted())
         }
     }
     
     
-    private static func prepare() {
+    private func getCellularDataRestricted()  -> Bool {
         if #available(iOS 9.0, *) {
             let cellState = CTCellularData.init()
-            cellState.cellularDataRestrictionDidUpdateNotifier = { (dataRestrictedState) in
-                if currentState != .restrictedStateUnknown { // State has changed - log to console
-                    print("cellularDataRestrictedState: " + "\(dataRestrictedState == .restrictedStateUnknown ? "unknown" : dataRestrictedState == .restricted ? "restricted" : "not restricted")")
-                }
-                currentState = dataRestrictedState
-            }
+            print("CellularRestricted check : " + "\(cellState.restrictedState == .restricted ? "restricted" : cellState.restrictedState == .restrictedStateUnknown ? "unknown" : "unrestricted")")
+            return  cellState.restrictedState == .restricted
         }
+        return false
     }
 }
